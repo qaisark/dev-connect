3e7be2182e9cc6d54c83b7c875214dd79176c0ce
qaisark <qaisarkasana6@gmail.com>
2019-08-25 16:27:10 +0500

    basic routes added

---
+++ routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+
+router.get('/', (req, res) => res.send('Auth Route'));
+
+module.exports = router;
+
+
