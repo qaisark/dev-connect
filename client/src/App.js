f8a59e5e3d20e5402cf83663d4998b3a92143c1d
qaisark <qaisarkasana6@gmail.com>
2019-08-27 22:19:33 +0500

    ui's added

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
+import Routes from './components/routing/Routes';
 
-//Redux 
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
 if (localStorage.token) {
@@ -27,17 +26,14 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/register" component={Register} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
-  )
+  );
 };
+
 export default App;
